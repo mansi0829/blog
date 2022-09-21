059b4c82cac055e331efdbe3124f3ca44bc91f3a
mansi0829 <mansigundre1@gmail.com>
2022-09-21 18:11:17 +0530

    user created

--- django_blogapp/settings.py
+++ django_blogapp/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
